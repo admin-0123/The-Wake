e2853cd2062c34a7e641b52d96b4da0692700a35
admin-0123 <williamjackson0123@outlook.com>
2023-01-09 22:10:48 -0300

    reduce assets size

--- components/advertise.vue
+++ components/advertise.vue
@@ -3,7 +3,7 @@
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-x-4 gap-y-0 py-20">
       <div class="col-span-1 sm:col-span-2 invisible sm:visible mx-8 pl-24">
         <div class="image-wrapper -rotate-3">
-          <img src="~/assets/image/section_3_item.png" alt="Section 3 Item" />
+          <img src="~/assets/image/section_3_item.jpg" alt="Section 3 Item" />
         </div>
       </div>
       <div class="col-span-1 sm:col-span-1 mx-10 sm:mx-0" id="advertise-right">
